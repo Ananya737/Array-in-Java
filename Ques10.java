b3cfe9c44a215e63974d14f65cbb274c4d6e46e9
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-11 07:45:45 +0530

    update

--- Ques10.java
+++ Ques10.java
@@ -1,3 +1,16 @@
 
 //Minimum
 
+import java.util.Scanner;
+public class Ques10{
+    public static void main(String args[]){
+        int arr[]=new int []{9,3,1,4,5,6};
+        int min=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
--- Ques9.java
+++ Ques9.java
@@ -1,2 +1,16 @@
 
 //Maximum 
+
+import java.util.Scanner;
+public class Ques9{
+    public static void main(String args[]){
+        int arr[]=new int []{9,3,1,4,13,5,6};
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
